a7977855949c8892f819aa1b09be936d718b80e2
kavya.pl <kavya.pl@gmail.com>
2019-05-28 06:07:35 +0530

    Http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ItemsService } from "./items.service";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -8,21 +9,83 @@ import { ItemsService } from "./items.service";
 })
 export class AppComponent {
   title = "client";
-  items: string[];
+  items: any[];
+  itemForm: FormGroup;
+  itemUpdateForm: FormGroup;
+  currentItemId: string;
   constructor(private itemService: ItemsService) {
-    itemService.getItems().subscribe(
-      res => {
-        console.log("RES", res);
-        this.items = res.items;
+    this.itemForm = new FormGroup({
+      name: new FormControl(),
+      type: new FormControl()
+    });
+    this.itemUpdateForm = new FormGroup({
+      name: new FormControl(),
+      type: new FormControl()
+    });
+
+    this.getItems();
+  }
+
+  getItems() {
+    this.itemService.getItems().subscribe(
+      (res: any) => {
+        console.log("RESULT", res);
+        this.items = res.result;
       },
       () => {},
       () => {}
     );
   }
-
-  create(newItem) {
-    this.itemService.createItems(newItem).subscribe(res => {
+  create() {
+    this.itemService.createItems(this.itemForm.value).subscribe(res => {
       console.log(res);
     });
+  }
+
+  delete(id: string) {
+    this.itemService.deleteItem(id).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  edit(item: any) {
+    // const name = item.name;
+    // const type = item.type;
+
+    const { name, type, _id } = item;
+    this.currentItemId = _id;
+    this.itemUpdateForm.setValue({ name, type });
+  }
+
+  update() {
+    console.log(this.itemUpdateForm.value);
+    this.itemService
+      .updateItem(this.currentItemId, this.itemUpdateForm.value)
+      .subscribe(
+        res => {
+          console.log(res);
+          console.log("ITEM ID", this);
+          for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i]._id === this.currentItemId) {
+              this.items[i].name = this.itemUpdateForm.value.name;
+              this.items[i].type = this.itemUpdateForm.value.type;
+            }
+          }
+        },
+        function(err) {
+          console.log(err);
+        },
+        () => {
+          let x = "Simple text";
+          console.log("ITEM ID from Complete block", this, x);
+
+          console.log("Done");
+        }
+      );
   }
 }
--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -5,15 +5,28 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class ItemsService {
-  constructor(private http: HttpClient) {}
+  BASEURL: string;
+  constructor(private http: HttpClient) {
+    this.BASEURL = "http://localhost:9090/items";
+  }
 
   getItems() {
-    return this.http.get("http://localhost:9090/items");
+    return this.http.get(`${this.BASEURL}/all`);
+    // http://localhost:9090/items/all
   }
 
   createItems(newItem) {
-    return this.http.post("http://localhost:9090/items", {
-      item: newItem
-    });
+    return this.http.post(`${this.BASEURL}/new`, newItem);
+    // http://localhost:9090/items/new
+  }
+
+  deleteItem(id: string) {
+    return this.http.delete(`${this.BASEURL}/delete/${id}`);
+    // http://localhost:9090/items/delete/ggfdfddf54545
+  }
+
+  updateItem(id: string, data: any) {
+    return this.http.put(`${this.BASEURL}/update/${id}`, data);
+    // http://localhost:9090/items/update/ggfdfddf54545
   }
 }
